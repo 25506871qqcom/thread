274d34b59c129e10cdcb48132f05673be49c6d92
lyp <25506871@qq.com>
2018-12-19 00:00:50 +0800

    ff

---
+++ src/main/java/lock/Lock.java
@@ -0,0 +1,35 @@
+package lock;
+
+public class Lock {
+    private static Object HALT_A = new Object();
+    private static Object HALT_B = new Object();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (HALT_A) {
+                try {
+                    Thread.sleep(50L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (HALT_B) {
+                    System.out.println("1");
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            synchronized (HALT_B) {
+
+                synchronized (HALT_A) {
+                    System.out.println("2");
+                }
+            }
+        }).start();
+
+
+
+//        jstack 35134
+
+    }
+}
